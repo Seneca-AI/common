b34cfdb8691fa1329514b8d64e620b1371fde0d6
Luca409 <Luca409@users.noreply.github.com>
2021-05-12 01:15:12 +0000

    auto generated commit to write proto_out

--- proto_out/python/api/type/aggregated_pb2.py
+++ proto_out/python/api/type/aggregated_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z\010api/type',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x10\x61ggregated.proto\x1a\x0c\x63ommon.proto\"\xd3\x01\n\x10\x44rivingCondition\x12\x37\n\x0e\x63ondition_type\x18\x01 \x01(\x0e\x32\x1f.DrivingCondition.ConditionType\x12\x10\n\x08severity\x18\x02 \x01(\x01\"t\n\rConditionType\x12\x0b\n\x07UNKNOWN\x10\x00\x12\t\n\x05NIGHT\x10\x01\x12\x08\n\x04RAIN\x10\x02\x12\x08\n\x04SNOW\x10\x03\x12\x07\n\x03ICE\x10\x04\x12\t\n\x05URBAN\x10\x05\x12\x0b\n\x07HIGHWAY\x10\x06\x12\t\n\x05RURAL\x10\x07\x12\x0b\n\x07TRAFFIC\x10\x08\"z\n\rDrivingPeriod\x12\x0f\n\x07user_id\x18\x01 \x01(\t\x12\x15\n\rstart_time_ms\x18\x02 \x01(\x03\x12\x13\n\x0b\x65nd_time_ms\x18\x03 \x01(\x03\x12,\n\x11\x64riving_condition\x18\x04 \x03(\x0b\x32\x11.DrivingCondition\"\xca\x01\n\x08SubEvent\x12\'\n\nevent_type\x18\x01 \x01(\x0e\x32\x13.SubEvent.EventType\x12\r\n\x05value\x18\x02 \x01(\x01\x12\x10\n\x08severity\x18\x03 \x01(\x01\x12\x1b\n\x06source\x18\x04 \x01(\x0b\x32\x0b.api.Source\"W\n\tEventType\x12\x0b\n\x07UNKNOWN\x10\x00\x12\x0f\n\x0bLANE_CHANGE\x10\x01\x12\x15\n\x11\x46\x41ST_ACCELERATION\x10\x02\x12\x15\n\x11\x46\x41ST_DECELERATION\x10\x03\"Y\n\x05\x45vent\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0f\n\x07user_id\x18\x02 \x01(\t\x12\x15\n\rstart_time_ms\x18\x03 \x01(\x03\x12\x1c\n\tsub_event\x18\x04 \x03(\x0b\x32\t.SubEventB\nZ\x08\x61pi/typeb\x06proto3'
+  serialized_pb=b'\n\x10\x61ggregated.proto\x1a\x0c\x63ommon.proto\"\xf0\x01\n\x10\x44rivingCondition\x12\x37\n\x0e\x63ondition_type\x18\x01 \x01(\x0e\x32\x1f.DrivingCondition.ConditionType\x12\x10\n\x08severity\x18\x02 \x01(\x01\x12\x1b\n\x06source\x18\x03 \x01(\x0b\x32\x0b.api.Source\"t\n\rConditionType\x12\x0b\n\x07UNKNOWN\x10\x00\x12\t\n\x05NIGHT\x10\x01\x12\x08\n\x04RAIN\x10\x02\x12\x08\n\x04SNOW\x10\x03\x12\x07\n\x03ICE\x10\x04\x12\t\n\x05URBAN\x10\x05\x12\x0b\n\x07HIGHWAY\x10\x06\x12\t\n\x05RURAL\x10\x07\x12\x0b\n\x07TRAFFIC\x10\x08\"\x86\x01\n\rDrivingPeriod\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0f\n\x07user_id\x18\x02 \x01(\t\x12\x15\n\rstart_time_ms\x18\x03 \x01(\x03\x12\x13\n\x0b\x65nd_time_ms\x18\x04 \x01(\x03\x12,\n\x11\x64riving_condition\x18\x05 \x03(\x0b\x32\x11.DrivingCondition\"\xca\x01\n\x08SubEvent\x12\'\n\nevent_type\x18\x01 \x01(\x0e\x32\x13.SubEvent.EventType\x12\r\n\x05value\x18\x02 \x01(\x01\x12\x10\n\x08severity\x18\x03 \x01(\x01\x12\x1b\n\x06source\x18\x04 \x01(\x0b\x32\x0b.api.Source\"W\n\tEventType\x12\x0b\n\x07UNKNOWN\x10\x00\x12\x0f\n\x0bLANE_CHANGE\x10\x01\x12\x15\n\x11\x46\x41ST_ACCELERATION\x10\x02\x12\x15\n\x11\x46\x41ST_DECELERATION\x10\x03\"Y\n\x05\x45vent\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0f\n\x07user_id\x18\x02 \x01(\t\x12\x15\n\rstart_time_ms\x18\x03 \x01(\x03\x12\x1c\n\tsub_event\x18\x04 \x03(\x0b\x32\t.SubEventB\nZ\x08\x61pi/typeb\x06proto3'
   ,
   dependencies=[common__pb2.DESCRIPTOR,])
 
@@ -81,8 +81,8 @@ _DRIVINGCONDITION_CONDITIONTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=130,
-  serialized_end=246,
+  serialized_start=159,
+  serialized_end=275,
 )
 _sym_db.RegisterEnumDescriptor(_DRIVINGCONDITION_CONDITIONTYPE)
 
@@ -116,8 +116,8 @@ _SUBEVENT_EVENTTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=488,
-  serialized_end=575,
+  serialized_start=530,
+  serialized_end=617,
 )
 _sym_db.RegisterEnumDescriptor(_SUBEVENT_EVENTTYPE)
 
@@ -144,6 +144,13 @@ _DRIVINGCONDITION = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='source', full_name='DrivingCondition.source', index=2,
+      number=3, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -158,7 +165,7 @@ _DRIVINGCONDITION = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=35,
-  serialized_end=246,
+  serialized_end=275,
 )
 
 
@@ -171,29 +178,36 @@ _DRIVINGPERIOD = _descriptor.Descriptor(
   create_key=_descriptor._internal_create_key,
   fields=[
     _descriptor.FieldDescriptor(
-      name='user_id', full_name='DrivingPeriod.user_id', index=0,
+      name='id', full_name='DrivingPeriod.id', index=0,
       number=1, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='start_time_ms', full_name='DrivingPeriod.start_time_ms', index=1,
-      number=2, type=3, cpp_type=2, label=1,
-      has_default_value=False, default_value=0,
+      name='user_id', full_name='DrivingPeriod.user_id', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='end_time_ms', full_name='DrivingPeriod.end_time_ms', index=2,
+      name='start_time_ms', full_name='DrivingPeriod.start_time_ms', index=2,
       number=3, type=3, cpp_type=2, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='driving_condition', full_name='DrivingPeriod.driving_condition', index=3,
-      number=4, type=11, cpp_type=10, label=3,
+      name='end_time_ms', full_name='DrivingPeriod.end_time_ms', index=3,
+      number=4, type=3, cpp_type=2, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='driving_condition', full_name='DrivingPeriod.driving_condition', index=4,
+      number=5, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -210,8 +224,8 @@ _DRIVINGPERIOD = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=248,
-  serialized_end=370,
+  serialized_start=278,
+  serialized_end=412,
 )
 
 
@@ -264,8 +278,8 @@ _SUBEVENT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=373,
-  serialized_end=575,
+  serialized_start=415,
+  serialized_end=617,
 )
 
 
@@ -317,11 +331,12 @@ _EVENT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=577,
-  serialized_end=666,
+  serialized_start=619,
+  serialized_end=708,
 )
 
 _DRIVINGCONDITION.fields_by_name['condition_type'].enum_type = _DRIVINGCONDITION_CONDITIONTYPE
+_DRIVINGCONDITION.fields_by_name['source'].message_type = common__pb2._SOURCE
 _DRIVINGCONDITION_CONDITIONTYPE.containing_type = _DRIVINGCONDITION
 _DRIVINGPERIOD.fields_by_name['driving_condition'].message_type = _DRIVINGCONDITION
 _SUBEVENT.fields_by_name['event_type'].enum_type = _SUBEVENT_EVENTTYPE
