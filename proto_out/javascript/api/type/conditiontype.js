5d6e0f3c956926c908bbaa234574961c29c5ff3a
Luca409 <Luca409@users.noreply.github.com>
2021-05-23 15:08:15 +0000

    auto generated commit to write proto_out

---
+++ proto_out/javascript/api/type/conditiontype.js
@@ -0,0 +1,30 @@
+// source: common.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.api.ConditionType');
+
+/**
+ * @enum {number}
+ */
+proto.api.ConditionType = {
+  NONE_CONDITION_TYPE: 0,
+  NIGHT: 1,
+  RAIN: 2,
+  SNOW: 3,
+  ICE: 4,
+  URBAN: 5,
+  HIGHWAY: 6,
+  RURAL: 7,
+  TRAFFIC: 8
+};
+
---
+++ proto_out/javascript/api/type/eventtype.js
@@ -0,0 +1,25 @@
+// source: common.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.api.EventType');
+
+/**
+ * @enum {number}
+ */
+proto.api.EventType = {
+  UNKNOWN_EVENT_TYPE: 0,
+  LANE_CHANGE: 1,
+  FAST_ACCELERATION: 2,
+  FAST_DECELERATION: 3
+};
+
